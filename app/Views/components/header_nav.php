e8caac1b7a3f96abf64d0b71303dee5fc133fc82
Krus <kattykitty176@gmail.com>
2025-03-14 03:28:03 +0800

    feat: Added gallery and gameplay routes

--- app/Views/components/header_nav.php
+++ app/Views/components/header_nav.php
@@ -1,22 +1,28 @@
 <header class="header">
     <!-- MISSING: LOGO -->
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark " data-bs-theme="dark">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark" data-bs-theme="dark">
     <div class="container-fluid">
-    <img src="<?php echo IMG.'JWS_Logo_Inverted.png'; ?>" alt="Mash" class="logo" width=120px height=120px>
+    <img src="<?php echo IMG.'JS&D-Logo.png'; ?>" alt="Mash" class="logo" width=120px height=120px>
     <div class="collapse navbar-collapse">
         <ul class = "navbar-nav me-auto mb-2 mb-sm-0">
             <li class="nav-item ms-5">
-                <a href="<?=base_url() ?>" class="nav-link fs-6">Home</a>
+                <a href="<?=base_url() ?>" class="nav-link fs-3">Home</a>
             </li>
             <li class="nav-item ms-5">
-                <a href="<?=base_url().'about' ?>" class="nav-link fs-6">About</a>
+                <a href="<?=base_url().'about' ?>" class="nav-link fs-3">About</a>
             </li>
             <li class="nav-item ms-5">
-                <a href="<?=base_url().'login' ?>" class="nav-link fs-6">Login</a>
+                <a href="<?=base_url().'gallery' ?>" class="nav-link fs-3">Gallery</a>
+            </li>
+            <li class="nav-item ms-5">
+                <a href="<?=base_url().'gameplay' ?>" class="nav-link fs-3">Gameplay</a>
+            </li>
+            <li class="nav-item ms-5">
+                <a href="<?=base_url().'login' ?>" class="nav-link fs-3">Login</a>
             </li>
         </ul>
         <form role="search">
-          <input class="form-control me-5" type="search" placeholder="Search" aria-label="Search">
+          <input class="form-control me-5 fs-3" type="search" placeholder="Search" aria-label="Search">
         </form>
     </div>
     </div>
--- app/Views/landing.php
+++ app/Views/landing.php
@@ -1,79 +1,15 @@
 <?= $this->extend('layout/main_layout') ?>
 <?= $this->section('pageContents') ?>
-<div class = "px-4 py-5 my-5 text-center">
-<header class="bg-dark text-white text-center py-5 rounded-3">
-        <h1 class ="display-1 fw-bold">Jed's Web Systems</h1>
-        <div class="col-lg-6 mx-auto">
-        <p class ="lead mb-4">A team composed of IT students working to create web pages in accordance to their requirements in their subject "Web Systems and Development."</p>
+<div class = "px-4 mx-4 text-center">
+<header class="bg-dark text-white text-center py-5 px-5 rounded-3 container-fluid">
+        <img src="<?php echo IMG.'Eden Island Frontier - Logo.png'; ?>" alt="GameLogo" class="mb-4 img-fluid">
+        <div class="mx-auto">
+        <p class ="lead mb-4 fw-bold fs-1">A 3D Strategy Simulation Game about the Implications of Climate Change and Economic Empowerment"</p>
         </div>
         <a href="<?=base_url().'about' ?>" class="btn btn-outline-light btn-lg px-4">About</a>
     </header>
 </div>
     <main class="container my-5">
-    <section>
-        <h2>Employees</h2>
-        <!-- Employee Table -->
-        <table class="table">
-            <thead class = "table-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
-    </section>
-    <section>
-        <h2>Employers</h2>
-        <table class="table">
-            <thead class = "table-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
-    </section>
+
     </main>
 <?= $this->endSection('pageContents') ?>
