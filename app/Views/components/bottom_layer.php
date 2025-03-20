70f1bbf53a28ab97d786bad2dfd29bc777a59387
Krus <kattykitty176@gmail.com>
2025-03-21 01:22:49 +0800

    feat: Enhance footer and header navigation styles, add new font styles, and improve gallery layout

--- app/Views/components/bottom_layer.php
+++ app/Views/components/bottom_layer.php
@@ -1,25 +1,26 @@
-    <footer class="bg-dark d-flex flex-wrap justify-content-between align-items-center mb-3 py-3 px-3" data-bs-theme="dark">
-        <p class="col-md-4 mb-0 text-body-secondary">© 2025 Jed's System Analysis and Design</p>
+    <footer class="footer bg-dark d-flex flex-wrap justify-content-between align-items-center mt-auto py-3 px-3" data-bs-theme="dark">
+        <p class="col-md-4 mb-0 text-body-secondary fs-4">© 2025 Jed's System Analysis and Design</p>
         <a href="<?=base_url() ?>" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <img src="<?php echo IMG.'JS&D-Logo.png'; ?>" alt="Mash" class="bi me-2" width=60px height=60px>
         </a>
         <ul class = "nav col-md-4 justify-content-end">
                 <li class="nav-item">
-                    <a href="<?=base_url() ?>" class="nav-link px-2 link-body-emphasis">Home</a>
+                    <a href="<?=base_url() ?>" class="nav-link px-3 link-body-emphasis fs-4">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?=base_url().'about' ?>" class="nav-link px-2 link-body-emphasis">About</a>
+                    <a href="<?=base_url().'about' ?>" class="nav-link px-3 link-body-emphasis fs-4">About</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?=base_url().'gallery' ?>" class="nav-link px-2 link-body-emphasis">Gallery</a>
+                    <a href="<?=base_url().'gallery' ?>" class="nav-link px-3 link-body-emphasis fs-4">Gallery</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?=base_url().'gameplay' ?>" class="nav-link px-2 link-body-emphasis">Gameplay</a>
+                    <a href="<?=base_url().'gameplay' ?>" class="nav-link px-3 link-body-emphasis fs-4">Gameplay</a>
                 </li>
         </ul>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
 </body>
 </html>
--- app/Views/gallery.php
+++ app/Views/gallery.php
@@ -1,4 +1,88 @@
 <?= $this->extend('layout/main_layout') ?>
 <?= $this->section('pageContents') ?>
-<p>Gallery</p>
+<main class="container py-5">
+<p class ="lead mb-4 fw-bold display-1 text-center">In-Game Asset Gallery</p>
+<div class="row" data-masonry="{&quot;percentPosition&quot;: true }" style="position: relative; height: 690px;">
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 0%; top: 0px;">
+      <div class="card">
+        <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'Main Character - Concept Art.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+        <div class="card-body">
+          <h5 class="card-title">Concept Art Main Character (Male/Female)</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 33.3333%; top: 0px;">
+      <div class="card p-3">
+        <figure class="p-3 mb-0">
+          <blockquote class="blockquote">
+            <p>A well-known quote, contained in a blockquote element.</p>
+          </blockquote>
+          <figcaption class="blockquote-footer mb-0 text-body-secondary">
+            Someone famous in <cite title="Source Title">Source Title</cite>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 66.6667%; top: 0px;">
+      <div class="card">
+        <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'VillagerNPC-ConceptArt.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+        <div class="card-body">
+          <h5 class="card-title">Concept Art Villager NPCs</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 33.3333%; top: 171px;">
+      <div class="card text-bg-primary text-center p-3">
+        <figure class="mb-0">
+          <blockquote class="blockquote">
+            <p>A well-known quote, contained in a blockquote element.</p>
+          </blockquote>
+          <figcaption class="blockquote-footer mb-0 text-white">
+            Someone famous in <cite title="Source Title">Source Title</cite>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 33.3333%; top: 310px;">
+      <div class="card text-center">
+      <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'Main House - Concept Art.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+        <div class="card-body">
+          <h5 class="card-title">Concept Art Main House</h5>
+          <p class="card-text">This card has a regular title and short paragraph of text below it.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 0%; top: 362px;">
+      <div class="card">
+      <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'482231496_2416748458674498_109483186446443634_n.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 66.6667%; top: 378px;">
+      <div class="card p-3 text-end">
+        <figure class="mb-0">
+          <blockquote class="blockquote">
+            <p>A well-known quote, contained in a blockquote element.</p>
+          </blockquote>
+          <figcaption class="blockquote-footer mb-0 text-body-secondary">
+            Someone famous in <cite title="Source Title">Source Title</cite>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 33.3333%; top: 488px;">
+      <div class="card">
+      <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'Resource Buildings - Concept Art.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+        <div class="card-body">
+          <h5 class="card-title">Concept Art Resource Buildings</h5>
+          <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
 <?= $this->endSection('pageContents') ?>
