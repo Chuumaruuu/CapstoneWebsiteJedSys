dbdbf7f28d35bb82f2845873d5c9228bc3484959
Krus <kattykitty176@gmail.com>
2025-10-14 22:59:21 +0800

    feat: installed Three.js library for better 3D web development

--- app/Views/gameplay.php
+++ app/Views/gameplay.php
@@ -1,5 +1,6 @@
 <?= $this->extend('layout/main_layout') ?>
 <?= $this->section('pageContents') ?>
+    <script type="module" src="<?php echo JS.'three.js'; ?>"></script>
 <main>
 <p>Gameplay</p>
 </main>
