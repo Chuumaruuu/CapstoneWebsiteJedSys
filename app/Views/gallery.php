dab4eb3750ebab3ca520d8e1277c2f79a65ba331
Krus <kattykitty176@gmail.com>
2025-10-29 03:21:23 +0800

    feat: Fixed gallery texts and positions

--- app/Views/gallery.php
+++ app/Views/gallery.php
@@ -45,7 +45,6 @@
         <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'Main Character - Concept Art.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <div class="card-body">
           <h5 class="card-title">Concept Art Main Character (Male/Female)</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
       </div>
     </div>
@@ -62,12 +61,10 @@
       </div>
     </div>
     <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 66.6667%; top: 0px;">
-      <div class="card">
+      <div class="card text-end">
         <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'VillagerNPC-ConceptArt.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <div class="card-body">
           <h5 class="card-title">Concept Art Villager NPCs</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
         </div>
       </div>
     </div>
@@ -84,19 +81,20 @@
       </div>
     </div>
     <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 33.3333%; top: 310px;">
-      <div class="card text-center">
+      <div class="card text-end">
       <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'Main House - Concept Art.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <div class="card-body">
           <h5 class="card-title">Concept Art Main House</h5>
-          <p class="card-text">This card has a regular title and short paragraph of text below it.</p>
-          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
         </div>
       </div>
     </div>
     <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 0%; top: 362px;">
-      <div class="card">
+      <div class="card text-center">
       <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'482231496_2416748458674498_109483186446443634_n.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
-      </div>
+      <div class="card-body">
+          <h5 class="card-title">Concept Art Resources</h5>
+        </div>
+    </div>
     </div>
     <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 66.6667%; top: 378px;">
       <div class="card p-3 text-end">
@@ -111,12 +109,10 @@
       </div>
     </div>
     <div class="col-sm-6 col-lg-4 mb-4" style="position: absolute; left: 33.3333%; top: 488px;">
-      <div class="card">
+      <div class="card text-center">
       <img class="bd-placeholder-img card-img-top" width="100%" height="100%" src="<?php echo IMG.'Resource Buildings - Concept Art.png'; ?>" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <div class="card-body">
           <h5 class="card-title">Concept Art Resource Buildings</h5>
-          <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
-          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
         </div>
       </div>
     </div>
