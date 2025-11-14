19fbaa3226c2e15067c30742ccd42e42a7b2afb4
Krus <kattykitty176@gmail.com>
2025-11-15 04:06:10 +0800

    refactor: code structure for improved readability and maintainability

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,23 +1,15 @@
 <?= $this->extend('layout/main_layout') ?>
 <?= $this->section('pageContents') ?>
 <main>
-<div class ="container py-4">
-<div class="p-5 mb-4 bg-body-tertiary border rounded-3">
+<div class ="container pt-5">
+  <div class="p-3 mb-4 bg-body-tertiary border rounded-3">
     <div class="container-fluid py-5">
-            <h1 class = "display-5 fw-bold">About Us</h1>
-            <p class="col-md-8 fs-4">Learn more about Jed's System Analysis and Design</p>
+      <h1 class = "display-1 fw-bold">Jed's System Analysis and Design</h1>
+      <p class="lead fs-3">A group of students who have a goal of creating a game based on agriculture to give importance on the implications of agriculture to climate change.</p>
     </div>
+  </div>
 </div>
-    <div class="row align-items-md-stretch">
-        <div class="col-md-6">
-            <div class = "h-100 p-5 text-bg-dark rounded-3">
-                <h2 class = "fw-bold">Our Mission</h2>
-                <p>To create a game based on agriculture to give importance on the implications of agriculture to climate change.</p>
-            </div>
-        </div>
-    </div>
-</div>
-<div class ="container py-4">
+<div class ="container pb-4">
     <div class="col-md-12">
         <h2 class = "fw-bold text-light display-2 border-bottom pb-3 mb-5">The Team</h2>
     </div>
@@ -93,7 +85,27 @@
         <div class="p-4 bg-body-tertiary border rounded-3 h-100">
           <h3 class="fw-bold">Far Eastern University - Institute of Technology</h3>
           <p class="fs-5">Padre Paredes St, Sampaloc, Manila, 1015 Metro Manila</p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.9113756070133!2d120.98604977400919!3d14.604124176992594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9f8b14eb259%3A0xad4d12caac9a068e!2sFEU%20Institute%20of%20Technology!5e0!3m2!1sen!2sph!4v1762862789531!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.9113756070133!2d120.98604977400919!3d14.604124176992594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9f8b14eb259%3A0xad4d12caac9a068e!2sFEU%20Institute%20of%20Technology!5e0!3m2!1sen!2sph!4v1762862789531!5m2!1sen!2sph" style="border:0; height:450px;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+</div>
+<div class ="container py-4">
+    <div class="row g-4">
+      <div class="col-12 col-md-4">
+        <div class="p-4 bg-body-tertiary border rounded-3 h-100">
+          <h3 class="fw-bold">Client Information</h3>
+          <p class="fs-5">If you have any questions or would like to work with us, please contact us at</p>
+          <p class="fs-5"><a href="mailto:inquiry@ndgm63.edu.ph">inquiry@ndgm63.edu.ph</a></p>
+          <hr>
+          <p class="fs-5">Phone: (02) 8361 7435<br>Office Hours: Mon–Fri, 9am–5pm</p>
+        </div>
+      </div>
+      <div class="col-12 col-md-8">
+        <div class="p-4 bg-body-tertiary border rounded-3 h-100">
+          <h3 class="fw-bold">Notre Dame of Greater Manila</h3>
+          <p class="fs-5">Madre Ignacia (12th) Avenue, 8th St., Brgy. 96, Grace Park East, Caloocan, Philippines</p>
+          <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.014517322348!2d120.98873517401024!3d14.655117375741796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b68169934159%3A0x82bb6ec41d63268c!2sNotre%20Dame%20of%20Greater%20Manila!5e0!3m2!1sen!2sph!4v1763150347685!5m2!1sen!2sph" style="border:0; height:450px;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
